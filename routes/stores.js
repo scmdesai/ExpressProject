85374d11eb82bbec49c7821023e13b740477807e
scmdesai <scmdesai@gmail.com>
2016-02-11 12:12:48 -0600

    storesList

--- routes/stores.js
+++ routes/stores.js
@@ -106,7 +106,7 @@ exports.findByStoreName = function(req, res) {
 	console.log(req.body) ;
 
    //res.send({id:req.params.storeName, businessName: "The Name", description: "description"});
-   stores.findOne({ _id: req.params.storeName }, function(err, stores) {
+   storesList.findOne({ _id: req.params.storeName }, function(err, stores) {
     if (err) {
       return res.send(err);
     }
