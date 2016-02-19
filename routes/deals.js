f531a849e0d2190f3c09abaf94a422b2ec55d602
scmdesai <scmdesai@gmail.com>
2016-02-18 19:47:11 -0600

    print date

--- routes/deals.js
+++ routes/deals.js
@@ -13,6 +13,7 @@ var snsClient = null ;
 exports.findAllDeals = function(req, res) {
 	//console.log("GET STORES") ;
 	var datetime = new Date();
+	
 	dealsList=[];
 
 	// switch to either use local file or AWS credentials depending on where the program is running
@@ -38,6 +39,7 @@ exports.findAllDeals = function(req, res) {
 	}
 	
 	console.log("SDB Client creation successful") ;
+	console.log('Today\'s date is ' + datetime);
 	var	params = {
 		SelectExpression: 'select * from MyDeals where DealStatus="Active" and DateEndDate < datetime', /* required */
 		ConsistentRead: true
