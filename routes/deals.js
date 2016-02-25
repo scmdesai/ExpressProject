1d8b40663dce87a67436908774cfabf69cb23a61
scmdesai <scmdesai@gmail.com>
2016-02-25 15:20:26 -0600

    server response to create deal

--- routes/deals.js
+++ routes/deals.js
@@ -218,7 +218,7 @@ exports.createNewDeal = function(req, res) {
 			});
 			
 			
-			res.status(200).send('{"success":true,"msg":"Deal Added Successfully"}') ;
+			res.status(200).send('{"success":true,"file":"+req.file+"\"","msg":"Deal Added Successfully"}') ;
 			//res.send('{"success":true,"msg":"Deal Added Successfully"}');
 			//res.set('Content-Type', 'text/plain');
 			
