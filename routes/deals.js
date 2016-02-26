312cb55110262106035b5441d5ab85016299af51
scmdesai <scmdesai@gmail.com>
2016-02-26 10:11:59 -0600

    failure to false

--- routes/deals.js
+++ routes/deals.js
@@ -218,7 +218,7 @@ exports.createNewDeal = function(req, res) {
 			});
 			
 			res.set('Content-Type', 'text/html');
-			res.status(200).send('"success":true,"msg":"Deal Added Successfully"') ;
+			res.status(200).send('{"failure":false,"msg":"Deal Added Successfully"}') ;
 			//res.send('{"success:true,msg:"Deal Added Successfully"}');
 			//res.set('Content-Type', 'text/plain');
 			
