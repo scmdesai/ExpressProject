a7bf9c9838df0b2c955431b789b651d23249bfae
scmdesai <scmdesai@gmail.com>
2016-03-10 14:18:14 -0600

    Revert "reg body deal param name change"
    This reverts commit 527dc0454734488d9d3b5b3597b5f9c9442692f6.

--- routes/deals.js
+++ routes/deals.js
@@ -157,12 +157,12 @@ exports.createNewDeal = function(req, res) {
 		},
 		{
 		  Name: 'CustID', /* required */
-		  Value: req.body.customerId, /* required */
+		  Value: req.body.CustID, /* required */
 		  Replace: false
 		},
 		{
 		  Name: 'BusinessName', /* required */
-		  Value: req.body.businessName, /* required */
+		  Value: req.body.BusinessName, /* required */
 		  Replace: false
 		}
 	],
