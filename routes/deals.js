5af4e00e76ea0e2ddc7cf7f24691e6ef1ed2653f
scmdesai <scmdesai@gmail.com>
2016-03-24 11:41:07 -0500

    deal date

--- routes/deals.js
+++ routes/deals.js
@@ -52,7 +52,7 @@ exports.findAllDeals = function(req, res) {
 	
 	
 	var	params = {
-		SelectExpression: 'select * from MyDeals where DealStatus ="Active" intersection DealEndDate >= "'+ dateString +'" order by DealEndDate',//+  datetime + "\"", /* required */
+		SelectExpression: 'select * from MyDeals where DealStatus ="Active" intersection DealEndDate >= "'+ datetime +'" order by DealEndDate',//+  datetime + "\"", /* required */
 		ConsistentRead: true
 		//NextToken: 'STRING_VALUE'
 	};
