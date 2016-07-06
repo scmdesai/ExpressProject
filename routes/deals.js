8055aaf1e3bcfb28229bea7bdd2892cf2e2e7d1d
scmdesai <scmdesai@gmail.com>
2016-07-05 23:55:40 -0500

    fileupload

--- routes/deals.js
+++ routes/deals.js
@@ -485,8 +485,8 @@ exports.uploadDealImage = function(req, res, next) {
 			cb( null, '' );
 		},
 		filename    : function( req, file, cb ) {
-			//cb( null, file.fieldname + '-' + Date.now() + ".jpg" );
-			cb( null, req.params.id + '-' + Date.now() + ".jpg" );
+			cb( null, file.fieldname + '-' + Date.now() + ".jpg" );
+			//cb( null, req.params.id + '-' + Date.now() + ".jpg" );
 		},
 		bucket      : 'appsonmobile.com/locallink/deals',
 		region      : 'us-west-2'
