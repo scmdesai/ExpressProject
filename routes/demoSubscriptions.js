ed7d4ffed23157579ccca9d2b64102c909dd67a7
scmdesai <scmdesai@gmail.com>
2016-08-09 20:09:29 -0500

    Revert "removed get call"
    This reverts commit 0c99d7e1bb79123ecfc8b86bb45901888832cf13.

--- routes/demoSubscriptions.js
+++ routes/demoSubscriptions.js
@@ -94,3 +94,76 @@ endDate.setDate(startDate.getDate() + 92);
 	});
 
 };
+
+exports.getSubscriptionStatus = function(req, res) {
+
+   //res.send({id:req.params.storeName, businessName: "The Name", description: req.body});
+   if(process.env.RUN_LOCAL=="TRUE") {
+		console.log("Loading local config credentials for accessing AWS");
+		AWS.config.loadFromPath('./config.json');
+	}
+	else {
+		console.log("Running on AWS platform. Using EC2 Metadata credentials.");
+		AWS.config.credentials = new AWS.EC2MetadataCredentials({
+			  httpOptions: { timeout: 10000 } // 10 second timeout
+		}); 
+		AWS.config.region = "us-west-2" ;
+	}
+
+	console.log("Credentials retrieval successful") ;
+	// Create an SDB client
+	console.log("Creating SDB Client") ;
+	var simpleDB = new AWS.SimpleDB() ;
+	console.log("SDB Client creation successful") ;
+	var	params = {
+		SelectExpression: 'select * from DemoMySubscriptions where CustomerId = ' + '"' + req.params.id + '"', /* required */
+		ConsistentRead: true
+		//NextToken: 'STRING_VALUE'
+	};
+	//console.log("Headers received:" + JSON.stringify(req.headers)) ;
+	var cb = req.query.callback;	
+	console.log("Callback URL is " + cb) ;
+
+	
+	console.log("Now retrieving data set from SDB") ;
+	simpleDB.select(params, function(err, data) {
+		if (err) {
+			console.log("ERROR calling AWS Simple DB!!!") ;
+			console.log(err, err.stack); // an error occurred
+		}
+		else     {
+			console.log("SUCCESS from AWS!") ;
+			console.log(JSON.stringify(data));           // successful response
+			console.log("Objects in the AWS data element:" ) ;
+			for(var name in data) {
+				console.log(name) ;
+			}
+			console.log("Now accessing Items element") ;
+			var items = data["Items"] ;
+			//console.log(items) ;
+			
+			
+			for(var i=0; i < items.length; i++) {
+				var item = items[i] ;	
+                console.log(item) ;				
+				var attributes = item["Attributes"] ;
+				subscriptionDetails[i] = new Subscription(attributes) ;
+				
+				
+			}
+			
+		}
+		console.log("Stores List is: " + subscriptionDetails);
+		var storesJsonOutput = JSON.stringify(subscriptionDetails) ;
+	    
+		
+		if(cb) {
+			res.send( cb + "(" + storesJsonOutput + ");" );
+		}
+		else {
+			res.send(storesJsonOutput) ;
+		}
+	});
+			
+   
+};
