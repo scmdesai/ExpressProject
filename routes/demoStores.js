bb0e916a7aeb7a9d1177b31030d875f97a37f24d
scmdesai <scmdesai@gmail.com>
2016-08-18 13:51:56 -0500

    topic anme'

--- routes/demoStores.js
+++ routes/demoStores.js
@@ -627,7 +627,7 @@ exports.createNewStore = function(req, res) {
 	var topicName = city.toString();
 	
 	var paramsTopic = {
-		Name: 'Naperville' /* required */
+		Name: 'LocalBuzzNaperville' /* required */
 	};
 	snsClient.createTopic(paramsTopic, function(err, data) {
 	  if (err) console.log(err, err.stack); // an error occurred
