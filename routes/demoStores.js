34c4f891351c5a7335f54514fc339b37c31bbd65
scmdesai <scmdesai@gmail.com>
2016-08-18 12:12:22 -0500

    snsClient name

--- routes/demoStores.js
+++ routes/demoStores.js
@@ -629,7 +629,7 @@ exports.createNewStore = function(req, res) {
 	var paramsTopic = {
 		Name: topicName /* required */
 	};
-	sns.createTopic(paramsTopic, function(err, data) {
+	snsClient.createTopic(paramsTopic, function(err, data) {
 	  if (err) console.log(err, err.stack); // an error occurred
 	  else     console.log(data);           // successful response
 	});
