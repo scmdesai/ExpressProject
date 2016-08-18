ed8eebd2cce3e7b10c4769a9f7ae9a561422e460
scmdesai <scmdesai@gmail.com>
2016-08-18 14:11:29 -0500

    topic name

--- routes/demoStores.js
+++ routes/demoStores.js
@@ -624,7 +624,7 @@ exports.createNewStore = function(req, res) {
 	
 	var city = req.body.city;
 	
-	var topicName = 'LocalBuzzNaperville';
+	var topicName = "'LocalBuzz"+ city+"'";
 	
 	var paramsTopic = {
 		Name: topicName /* required */
