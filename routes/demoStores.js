512bf2af2f141dbafaa274c8654eeb86207c7324
scmdesai <scmdesai@gmail.com>
2016-08-18 13:35:41 -0500

    topic name

--- routes/demoStores.js
+++ routes/demoStores.js
@@ -624,10 +624,10 @@ exports.createNewStore = function(req, res) {
 	
 	var city = req.body.city;
 	
-	var topicName = 'LocalBuzz'+city;
+	var topicName = city;
 	
 	var paramsTopic = {
-		Name: 'LocalBuzz'+city /* required */
+		Name: city /* required */
 	};
 	snsClient.createTopic(paramsTopic, function(err, data) {
 	  if (err) console.log(err, err.stack); // an error occurred
