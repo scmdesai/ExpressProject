b331ecfb04c77d9286020d6d1ee885527e111dab
scmdesai <scmdesai@gmail.com>
2016-08-18 13:19:27 -0500

    Revert "topic"
    This reverts commit c419e7906a193fdc63df30b87b4df48d7ef6626c.

--- routes/demoStores.js
+++ routes/demoStores.js
@@ -627,7 +627,7 @@ exports.createNewStore = function(req, res) {
 	
 	
 	var params = {
-		Topic: "LocalBuzzTest" /* required */
+		Topic Name: "LocalBuzzTest" /* required */
 	};
 	snsClient.createTopic(params, function(err, data) {
 	  if (err) console.log(err, err.stack); // an error occurred
