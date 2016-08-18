924eb504db0d6a8deb8377b9622d41fef92b5335
scmdesai <scmdesai@gmail.com>
2016-08-18 13:56:16 -0500

    tpic name

--- routes/demoStores.js
+++ routes/demoStores.js
@@ -624,10 +624,10 @@ exports.createNewStore = function(req, res) {
 	
 	var city = req.body.city;
 	
-	var topicName = city.toString();
+	var topicName = 'LocalBuzz'+city.toString();
 	
 	var paramsTopic = {
-		Name: 'LocalBuzzNaperville' /* required */
+		Name: topicName /* required */
 	};
 	snsClient.createTopic(paramsTopic, function(err, data) {
 	  if (err) console.log(err, err.stack); // an error occurred
