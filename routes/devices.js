3c71dac996947f64f7d0c7b9122ddbbcbee230e0
scmdesai <scmdesai@gmail.com>
2016-08-19 00:39:10 -0500

    test get

--- routes/devices.js
+++ routes/devices.js
@@ -1,5 +1,4 @@
 var AWS = require('aws-sdk');
-var request = require('request');
 var snsClient = null ;
 
 exports.registerNewDevice = function(req, res) {
@@ -75,28 +74,20 @@ exports.registerNewDevice = function(req, res) {
 			console.log(data);           // successful response
 			endPointARN = data.EndpointArn  ;
 			//res.status(200).send('{"success":true,"msg":"Device Registered Successfully"}') ;
-			request("http://api.geonames.org/findNearbyPostalCodesJSON?postalcode="+json.userLocation+"&country=US&radius=30&maxRows=500&username=1234_5678", function (error, response, body) {
-			  for(postalcodes in body.postalCodes){
-				  topicArn = 'arn:aws:sns:us-west-2:861942316283:LocalBuzz'+ body.postalCodes[postalcodes].placeName;
-				  console.log("Endpoint ARN is: " + endPointARN) ;
-				  var params = {
-					Protocol: 'application', /* required */
-					TopicArn: topicArn,//'arn:aws:sns:us-west-2:861942316283:LocalLinkNotification', /* required */
-					Endpoint: data.EndpointArn
-				 };
-				 snsClient.subscribe(params, function(err, data) {
-					if (err) {
-						console.log(err, err.stack); // an error occurred
-						res.status(500).send('{"success":false,"msg":"Suscription to Topic Failed"}') ;
-					}	
-					else {
-						console.log(data);           // successful response
-						res.status(200).send('{"success":true,"msg":"Subscribed to Topic Successfully"}') ;
-					}
-						
-				});
-			}  
-			});
+			xhr = new XMLHttpRequest();
+			var url = "http://api.geonames.org/findNearbyPostalCodesJSON?postalcode="+json.userLocation+"&country=US&radius=30&maxRows=500&username=1234_5678";
+			xhr.open("GET", url, true);
+			//xhr.setRequestHeader("Content-type", "application/json");
+			xhr.onreadystatechange = function () { 
+				if (xhr.readyState == 4 && xhr.status == 200) {
+					var jsonArea = JSON.parse(xhr.responseText);
+					console.log(jsonArea.success + ", " + jsonArea.msg) ;
+				}
+			}
+			
+			
+			  
+			
 	  
 			/*if(json.userLocation=="60540"){
 			 topicArn = 'arn:aws:sns:us-west-2:861942316283:LocalBuzzNaperville';
