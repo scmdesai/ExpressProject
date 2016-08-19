e1c5e396afd349d1f71c2d1cd9045d90bd01256f
scmdesai <scmdesai@gmail.com>
2016-08-19 01:56:53 -0500

    postal codes

--- routes/devices.js
+++ routes/devices.js
@@ -78,10 +78,8 @@ exports.registerNewDevice = function(req, res) {
 			request("http://api.geonames.org/findNearbyPostalCodesJSON?postalcode=60504&country=US&radius=30&maxRows=500&username=1234_5678", 
 			function (error, response, body) {
 		    if (!error && response.statusCode == 200) {
-			    for(var name in body) {
-					console.log(name) ;
-				}
-				console.log(); // Show the HTML for the Google homepage.
+			    
+				console.log(body["postalCodes"]); // Show the HTML for the Google homepage.
 				/*for(var i=0;i<100;i++){
 				  topicArn = 'arn:aws:sns:us-west-2:861942316283:LocalBuzz'+ postalCodes[i].placeName;
 				  console.log("Endpoint ARN is: " + endPointARN) ;
