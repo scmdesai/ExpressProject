4c7d105e1880ee82f66a9180d2376431b3019f13
scmdesai <scmdesai@gmail.com>
2016-08-18 22:23:19 -0500

    syntax

--- routes/demoDeals.js
+++ routes/demoDeals.js
@@ -262,7 +262,7 @@ exports.createNewDeal = function(req, res) {
 				"APNS_SANDBOX":"{\"aps\":{\"alert\":\"New buzz from " + req.body.businessName + " : " + req.body.DealName + "\"}}", 
 				"GCM": "{ \"data\": { \"message\": \"New buzz from "  + req.body.businessName + " : " + req.body.DealName + "\"} }"
 			};
-			vvar topicArn= (req.body.TopicArn).toString();
+			var topicArn= (req.body.TopicArn).toString();
 			
 			var params = {
 				Message: JSON.stringify(message),
