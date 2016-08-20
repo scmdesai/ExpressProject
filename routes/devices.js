74c64f82a3488fda990641521917b39bc1a840b9
scmdesai <scmdesai@gmail.com>
2016-08-20 08:47:08 -0500

    revert back changes

--- routes/devices.js
+++ routes/devices.js
@@ -96,193 +96,175 @@ exports.registerNewDevice = function(req, res) {
 			endPointARN = data.EndpointArn  ;
 			var listOfTopics = [];
 			
+			/* List All Topics to get the list of cities that are registered with Local Buzz */
+			snsClient.listTopics(params1={}, function(err, data)
+			{
+				if (err) console.log(err, err.stack); // an error occurred
+				else  
+				{
+					//console.log(data);           // successful response
+					//var parseList  = JSON.parse(data);
+					var i=0;
+					while(data.Topics[i]){
+						listOfTopics.push(data.Topics[i].TopicArn);
+						i++;
+						//If the list of topics is more than 100,use Next token
+					}
+				}
+			});
 			/*At this point,we have the endpointARN,query the SDB table to get the associated subscriptions*/
-					
-					
-					/* Get current list of active subscriptions for the endpoint and unsubscribe them*/
+			
+			
+			/* Get current list of active subscriptions for the endpoint and unsubscribe them*/
 			
 			var	params2 = 
-					{
-						SelectExpression: 'select SubscriptionARN from EndpointARNs where EndpointARN = ' + '"' + endPointARN + '"', /* required */
-						ConsistentRead: true
-						//NextToken: 'STRING_VALUE'
-					};
+			{
+				SelectExpression: 'select SubscriptionARN from EndpointARNs where EndpointARN = ' + '"' + endPointARN + '"', /* required */
+				ConsistentRead: true
+				//NextToken: 'STRING_VALUE'
+		   };
 		
 
 		
-					console.log("Now retrieving data set from SDB") ;
-					simpleDB.select(params2, function(err, dataSDB) {
-						if (err) {
-							console.log("ERROR calling AWS Simple DB!!!") ;
-							console.log(err, err.stack); // an error occurred
-						}
-						else     
-						{
-							console.log("SUCCESS from AWS!") ;
-							var items = dataSDB["Items"];
-							console.log(dataSDB);
-							if(items)
-							{
-								for(var k=0; k < items.length;k++) {
-									var item = items[k] ;	
-									console.log(item) ;				
-									var attributes = item["Attributes"] ;
-									var attr = attributes[0] ; // we are only getting the SubscriptionARN
-									var attrName = attr["Name"] ;  // SubscriptionARN
-									var attrValue = attr["Value"] ; // value of the SubscriptionARN to pass to unsubscribe call
+			console.log("Now retrieving data set from SDB") ;
+			simpleDB.select(params2, function(err, data) {
+				if (err) {
+					console.log("ERROR calling AWS Simple DB!!!") ;
+					console.log(err, err.stack); // an error occurred
+				}
+				else     
+				{
+					console.log("SUCCESS from AWS!") ;
+					var items = data["Items"];
+					if(items)
+					{
+						for(var i=0; i < items.length; i++) {
+							var item = items[i] ;	
+							console.log(item) ;				
+							var attributes = item["Attributes"] ;
+							var attr = attributes[0] ; // we are only getting the SubscriptionARN
+							var attrName = attr["Name"] ;  // SubscriptionARN
+							var attrValue = attr["Value"] ; // value of the SubscriptionARN to pass to unsubscribe call
+							
+							console.log('SubscriptionARN to unsubscribe is : '+ attrValue);
+							var params3 = {
+							  SubscriptionArn: attrValue /* required */
+							};
+							snsClient.unsubscribe(params3, function(err, data) {
+								if (err) {
+									console.log(err, err.stack); // an error occurred
+									//res.status(500).send('{"success":false,"msg":"Suscription to Topic Failed"}') ;
 									
-									console.log('SubscriptionARN to unsubscribe is : '+ attrValue);
-									var params3 = {
-									  SubscriptionArn: attrValue /* required */
-									};
-									snsClient.unsubscribe(params3, function(err, dataUnsubscribe) {
+								}	
+								else {
+									console.log(data);           // successful response
+									//res.status(200).send('{"success":true,"msg":"Subscribed to Topic Successfully"}') ;
+								}
+								
+							});
+							
+						}
+					}
+				}
+			});
+					
+		/* Find the list of cities within 30 miles of the user */
+			request("http://api.geonames.org/findNearbyPostalCodesJSON?postalcode=60504&country=US&radius=30&maxRows=500&username=1234_5678", 
+				function (error, response, body) {
+					if (!error && response.statusCode == 200) {
+					
+						var jsonArea = JSON.parse(body); // Show the HTML for the Google homepage.
+					
+						for(var i=0;i<500;i++){
+						  if(jsonArea.postalCodes[i]){
+						 // console.log(jsonArea.postalCodes[i].placeName);
+						  topicArn = 'arn:aws:sns:us-west-2:861942316283:LocalBuzz'+ jsonArea.postalCodes[i].placeName + jsonArea.postalCodes[i].adminCode1;
+						  //console.log("Endpoint ARN is: " + endPointARN) ;
+						  
+						  
+						  /* Subscribe the user to the cities that are registered with Local Buzz */
+						   for(var j=0;j< listOfTopics.length ;j++)
+							{
+								if( topicArn == listOfTopics[j])
+								{
+									var params = {
+									Protocol: 'application', /* required */
+									TopicArn: topicArn,//'arn:aws:sns:us-west-2:861942316283:LocalLinkNotification', /* required */
+									Endpoint: data.EndpointArn
+								 };
+								//console.log('Subscribing to: ' + 'LocalBuzz'+ jsonArea.postalCodes[i].placeName + jsonArea.postalCodes[i].adminCode1) ;
+									snsClient.subscribe(params, function(err, data)
+									{
 										if (err) {
 											console.log(err, err.stack); // an error occurred
 											//res.status(500).send('{"success":false,"msg":"Suscription to Topic Failed"}') ;
 											
 										}	
-										else {
-											//console.log(dataUnsubscribe);           // successful response
-											//res.status(200).send('{"success":true,"msg":"Subscribed to Topic Successfully"}') ;
-										}
-										
-									});
-									
-								}
-							}
-						}
-					});
-			
-			/* List All Topics to get the list of cities that are registered with Local Buzz */
-			snsClient.listTopics(params1={}, function(err, dataListTopics)
-			{
-				if (err) console.log(err, err.stack); // an error occurred
-				else  
-				{
-					console.log(dataListTopics);           // successful response
-					//var parseList  = JSON.parse(data);
-					var i=0;
-					while(dataListTopics.Topics[i]){
-						//listOfTopics.push(dataListTopics.Topics[i].TopicArn);
-						
-						console.log(dataListTopics.Topics[i]);
-						console.log(dataListTopics.Topics[i].TopicArn);
-						//If the list of topics is more than 100,use Next token
-					//}
-				//}
-			//});
-					
-					
-					
-					
-				/* Find the list of cities within 30 miles of the user */
-					request("http://api.geonames.org/findNearbyPostalCodesJSON?postalcode=60504&country=US&radius=30&maxRows=500&username=1234_5678", 
-						function (error, response, body) {
-							if (!error && response.statusCode == 200) {
-							
-								var jsonArea = JSON.parse(body); // Show the HTML for the Google homepage.
-							
-								for(var m=0;m<500;m++){
-								  if(jsonArea.postalCodes[m]){
-								 // console.log(jsonArea.postalCodes[i].placeName);
-								  topicArn = 'arn:aws:sns:us-west-2:861942316283:LocalBuzz'+ jsonArea.postalCodes[m].placeName + 
-								  jsonArea.postalCodes[m].adminCode1;
-								  //console.log("Endpoint ARN is: " + endPointARN) ;
-								  
-								  
-								  /* Subscribe the user to the cities that are registered with Local Buzz */
-								   //for(var n=0;n< listOfTopics.length ;n++)
-									//{
-										if( topicArn == dataListTopics.Topics[i].TopicArn)
+										else 
 										{
-											var params = {
-											Protocol: 'application', /* required */
-											TopicArn: topicArn,//'arn:aws:sns:us-west-2:861942316283:LocalLinkNotification', /* required */
-											Endpoint: data.EndpointArn
-										 };
-										//console.log('Subscribing to: ' + 'LocalBuzz'+ jsonArea.postalCodes[i].placeName + jsonArea.postalCodes[i].adminCode1) ;
-											snsClient.subscribe(params, function(err, dataSubscribe)
+											console.log('Subscription ARN is : ' + data.SubscriptionArn);           // successful response
+											//res.status(200).send('{"success":true,"msg":"Subscribed to Topic Successfully"}') ;
+											/* Insert the endpoint and subscription into the SDB table ***/
+											var uuid1 = uuid.v1();
+											console.log("Generated uuid for itemName " + uuid1) ;
+											var params1 = {
+											  Attributes: [ /* required */
+												{
+												  Name: 'EndpointARN', /* required */
+												  Value: endPointARN, /* required */
+												  Replace: false
+												},
+												{
+												  Name: 'SubscriptionARN', /* required */
+												  Value: data.SubscriptionArn, /* required */
+												  Replace: false
+												}
+											],
+											  DomainName: 'EndpointARNs', /* required */
+											  ItemName: uuid1, /* required */
+											  Expected: {
+												Exists: false,
+												Name: 'EndpointARN'
+											  }
+											};
+					
+											console.log("Now inserting new row into EndpointARNs domain") ;
+											simpleDB.putAttributes(params1, function(err, data) 
 											{
 												if (err) {
+													console.log("Error inserting record") ;
 													console.log(err, err.stack); // an error occurred
-													//res.status(500).send('{"success":false,"msg":"Suscription to Topic Failed"}') ;
-													
-												}	
-												else 
-												{
-													console.log('Subscription ARN is : ' + dataSubscribe.SubscriptionArn);           // successful response
-													//res.status(200).send('{"success":true,"msg":"Subscribed to Topic Successfully"}') ;
-													/* Insert the endpoint and subscription into the SDB table ***/
-													var uuid1 = uuid.v1();
-													console.log("Generated uuid for itemName " + uuid1) ;
-													var params1 = {
-													  Attributes: [ /* required */
-														{
-														  Name: 'EndpointARN', /* required */
-														  Value: endPointARN, /* required */
-														  Replace: false
-														},
-														{
-														  Name: 'SubscriptionARN', /* required */
-														  Value: dataSubscribe.SubscriptionArn, /* required */
-														  Replace: false
-														}
-													],
-													  DomainName: 'EndpointARNs', /* required */
-													  ItemName: uuid1, /* required */
-													  Expected: {
-														Exists: false,
-														Name: 'EndpointARN',
-														
-													  }
-													};
-							
-													console.log("Now inserting new row into EndpointARNs domain") ;
-													simpleDB.putAttributes(params1, function(err, dataInsertSDB) 
-													{
-														if (err) {
-															console.log("Error inserting record") ;
-															console.log(err, err.stack); // an error occurred
-															//res.status(500).send('{ "success": false, "msg": "Error adding buzz: "' + err + "}") ;
-															
-														}
-														else  
-														{
-															console.log("Record inserted successfully") ;
-															console.log(dataInsertSDB);           // successful response
-															
-
-															
-															
-															
-															
-														}
-													});
-												/**************************************************************************/
-												
+													//res.status(500).send('{ "success": false, "msg": "Error adding buzz: "' + err + "}") ;
 												}
-										
+												else  
+												{
+													console.log("Record inserted successfully") ;
+													console.log(data);           // successful response
+
+													
+													
+													
+													
+												}
 											});
-											
-										}
+										/**************************************************************************/
 										
-									//}
-							  
+										}
+								
+									});
 								}
-						  
 							}
-							
+					  
 						}
-						  
-					});
-	
-			     i++;
+				  
+					}
+					res.status(200).send('{"success":true,"msg":"Subscribed to Topic Successfully"}') ;
 				}
-				}
+				  
 			});
-		}
-		res.status(200).send('{"success":true,"msg":"Subscribed to Topic Successfully"}') ;
-	});
 	
+			
+		}
+	});
 };	
 
