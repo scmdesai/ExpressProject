b4f1584704151308cddabe6d324b78e6da6ce98c
scmdesai <scmdesai@gmail.com>
2016-08-20 15:39:42 -0500

    delete

--- routes/devices.js
+++ routes/devices.js
@@ -96,7 +96,7 @@ exports.registerNewDevice = function(req, res) {
 			endPointARN = data.EndpointArn  ;
 			var listOfTopics = [];
 			var listOfCitiesAlreadySubscribed = [];
-			var listOfUnsubscribedArns =[];
+			
 			
 			/* List All Topics to get the list of cities that are registered with Local Buzz */
 			snsClient.listTopics(params1={}, function(err, data)
@@ -147,7 +147,7 @@ exports.registerNewDevice = function(req, res) {
 							var attr = attributes[0] ; // we are only getting the SubscriptionARN
 							var attrName = attr["Name"] ;  // SubscriptionARN
 							var attrValue = attr["Value"] ; // value of the SubscriptionARN to pass to unsubscribe call
-							listOfUnsubscribedArns.push(attrName);
+                            
 							//console.log('SubscriptionARN to unsubscribe is : '+ attrValue);
 							var params3 = {
 							  SubscriptionArn: attrValue /* required */
@@ -172,36 +172,43 @@ exports.registerNewDevice = function(req, res) {
 					}
 				}
 			});
-	console.log('Unsubscribed List length: ' + listOfUnsubscribedArns.length);		
-	for(var i=0;i< listOfUnsubscribedArns.length;i++){		
-		var params4 = {
-		  
-		  DomainName: 'EndpointARNs', /* required */
-		  ItemName: listOfUnsubscribedArns[i] /* required */
-		  
-		};
-		
-		console.log("Now deleting  rows in EndpointARNs domain") ;
-		simpleDB.deleteAttributes(params4, function(err, data) {
-			 
-			if (err) {
-				console.log("Error deleting Buzz") ;
-				console.log(err, err.stack); // an error occurred
-				//res.status(500).send('"success": false, "msg": "Error deleting buzz: " + err') ;
-				/*res.status(500).send('<script type=\"text/javascript\"> alert( "Error deleting buzz:" + err );</script>');*/
-			}
-			else  {
-				//console.log("Deal deleted successfully") ;
-				//console.log(data);           // successful response
-				//res.status(201).send('"success": true, "msg": "Deal deleted successfully"') ;
-				//res.status(200).send('{ "success": true, "msg": "Buzz Deleted" }') ;
-				//res.status(200).send('<script type=\"text/javascript\"> alert("Deal deleted successfully") ;</script>' ) ;
-				
-				
-			}
-		});
-	}
     
+			
+	var params4 = {
+	  Attributes: [ 
+		
+		{
+		  Name: 'SubscriptionARN' 
+		},
+		{
+		  Name: 'EndpointARN' 
+		}
+		
+	],
+	  DomainName: 'EndpointARNs', /* required */
+	  ItemName:  itemName() /* required */
+	  
+	};
+	
+	console.log("Now deleting a row in DemoMyDeals domain") ;
+	simpleDB.deleteAttributes(params4, function(err, data) {
+	     
+		if (err) {
+			console.log("Error deleting Buzz") ;
+			console.log(err, err.stack); // an error occurred
+			res.status(500).send('"success": false, "msg": "Error deleting buzz: " + err') ;
+			/*res.status(500).send('<script type=\"text/javascript\"> alert( "Error deleting buzz:" + err );</script>');*/
+		}
+		else  {
+			console.log("Deal deleted successfully") ;
+			console.log(data);           // successful response
+			//res.status(201).send('"success": true, "msg": "Deal deleted successfully"') ;
+			res.status(200).send('{ "success": true, "msg": "Buzz Deleted" }') ;
+			//res.status(200).send('<script type=\"text/javascript\"> alert("Deal deleted successfully") ;</script>' ) ;
+			
+			
+		}
+	});
 	
 					
 		/* Find the list of cities within 30 miles of the user */
