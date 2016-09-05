a8fbf77b8a8a8fd836e7d4ff7f4438651ca0f98e
scmdesai <scmdesai@gmail.com>
2016-09-05 00:03:34 -0500

    removed the jsonArea.postalCodes.length.the app was not loading stores

--- routes/stores.js
+++ routes/stores.js
@@ -336,7 +336,7 @@ exports.filterByLocation = function(req, res) {
 					
 						var jsonArea = JSON.parse(body); // Show the HTML for the Google homepage.
 						console.log('Length of Json object is : ' +jsonArea.postalCodes.length);
-						for(var i=0;i<jsonArea.postalCodes.length;i++){
+						for(var i=0;i<500;i++){
 						  if(jsonArea.postalCodes[i]){
 							var zipcode = jsonArea.postalCodes[i].postalCode;
 
