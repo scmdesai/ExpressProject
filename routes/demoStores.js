2ed6574087413a9495422d1afbb5bc35e09e4956
scmdesai <scmdesai@gmail.com>
2016-09-07 08:35:49 -0500

    find by email

--- routes/demoStores.js
+++ routes/demoStores.js
@@ -681,6 +681,106 @@ exports.updateOnlyBusinessInfo = function(req, res) {
 	
 };
 
+exports.findByLoginEmail = function(req, res) {
+	console.log("GET STORE BY NAME") ;
+	console.log(req.body) ;
+	
+
+   //res.send({id:req.params.storeName, businessName: "The Name", description: req.body});
+   if(process.env.RUN_LOCAL=="TRUE") {
+		console.log("Loading local config credentials for accessing AWS");
+		AWS.config.loadFromPath('./config.json');
+	}
+	else {
+		console.log("Running on AWS platform. Using EC2 Metadata credentials.");
+		AWS.config.credentials = new AWS.EC2MetadataCredentials({
+			  httpOptions: { timeout: 10000 } // 10 second timeout
+		}); 
+		AWS.config.region = "us-west-2" ;
+	}
+
+	console.log("Credentials retrieval successful") ;
+	// Create an SDB client
+	console.log("Creating SDB Client") ;
+	var simpleDB = new AWS.SimpleDB() ;
+	console.log("SDB Client creation successful") ;
+	var	params = {
+		SelectExpression: 'select * from DemoMyCustomers where loginEmail = ' + '"' + req.params.email + '"', /* required */
+		ConsistentRead: true
+		//NextToken: 'STRING_VALUE'
+	};
+	console.log("params: " + req.params.email) ;
+	var cb = req.query.callback;	
+	console.log("Callback URL is " + cb) ;
+
+	
+	console.log("Now retrieving data set from SDB") ;
+	simpleDB.select(params, function(err, data) {
+		if (err) {
+			console.log("ERROR calling AWS Simple DB!!!") ;
+			console.log(err, err.stack); // an error occurred
+		}
+		else     {
+			console.log("SUCCESS from AWS!") ;
+			console.log(JSON.stringify(data));           // successful response
+			console.log("Objects in the AWS data element:" ) ;
+			for(var name in data) {
+				console.log(name) ;
+			}
+			console.log("Now accessing Items element") ;
+			var items = data["Items"] ;
+			//console.log(items) ;
+			
+			// loginEmail is unique
+			//for(var i=0; i < items.length; i++) {
+			if(items){
+				var item = items[0] ;	
+                console.log(item) ;				
+				var attributes = item["Attributes"] ;
+				storeDetails = new Store(attributes) ;
+				
+				/*
+				//console.log(attributes) ;
+				for(var j in attributes) {
+					var attr = attributes[j];
+					//console.log(attr) ;
+					var nameAttr = attr["Name"];
+					var valueAttr = attr["Value"];
+					//console.log(nameAttr + ": " + valueAttr );
+					var storesJsonOutput;
+					storesJsonOutput = (nameAttr + ": " + valueAttr + "\n" );
+					res.write(storesJsonOutput);
+					
+				
+				}
+			
+				res.end() ;*/	
+				/*var store = new Store() ;
+				for(var j=0; j < attributes.length; j++) {
+					var attribute = attributes[j] ;
+					if(attribute["Name"] == "BusinessName") {
+						store.businessName = attribute["Value"] ;
+					}
+				}
+				storesList[i] = store ;*/
+				//console.log(attributes) ;
+			}
+			
+		}
+		console.log("Stores List is: " + storeDetails);
+		var storesJsonOutput = JSON.stringify(storeDetails) ;
+	    
+		
+		if(cb) {
+			res.send( cb + "(" + storesJsonOutput + ");" );
+		}
+		else {
+			res.send(storesJsonOutput) ;
+		}
+	});
+			
+   
+};
 
 
 exports.updateBusinessInfo = function(req, res) {
