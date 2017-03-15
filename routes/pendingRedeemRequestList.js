b69dabf5e6d03616df5da5cc8535b5d676763f1a
scmdesai <scmdesai@gmail.com>
2017-03-15 00:59:28 -0500

    redeem

--- routes/pendingRedeemRequestList.js
+++ routes/pendingRedeemRequestList.js
@@ -161,7 +161,7 @@ exports.approveRedeemRequest = function(req, res) {
 		}
 	],
 	  DomainName: 'CouponCodesForLocalBuzz', /* required */
-	  ItemName: req.params.id/* required */
+	  ItemName: req.body.itemName/* required */
 	  
 	};
 	
