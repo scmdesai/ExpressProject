829bbd509bdae15dc8c29f72fb8783bdfa135f93
scmdesai <scmdesai@gmail.com>
2017-03-16 13:42:28 -0500

    server error handling

--- routes/deals.js
+++ routes/deals.js
@@ -1086,7 +1086,7 @@ exports.createOfferCode = function(req, res) {
 				console.log("Error inserting record") ;
 				console.log(err, err.stack); // an error occurred
 				//res.send('{"msg": "Error! Please try again","err":"'+err+'"}') ;
-				res.send('{"success":false,"msg":"Error! Please try again"}');
+				res.send('{"success":false,"msg":"'+err+'"}');
 			}
 			else  {
 				
